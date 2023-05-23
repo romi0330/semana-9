24a84d7abb58d0014c358e62cda6b6965e590faf
romi0330 <romina03villavicencio@gmail.com>
2023-05-23 13:06:57 -0500

    final final

--- aleatorio final.c
+++ aleatorio final.c
@@ -4,11 +4,13 @@
 
 int main (int argc, char *argv[]) {
     int num1,num2;
+    // se pide a usuario igresar las dmensiones de la matriz
     printf("Ingrese numero de columnas: \n");
     scanf("%d", &num1);
     printf("Ingrese numero de filas: \n");
     scanf("%d", &num2);
     int matriz[num2][num1];
+    //se utiliza srand para cada vz que se corre salgan numeros diferentes
     srand(time(NULL));
     for (int i = 0; i < num2; i++) {
         for (int j = 0; j < num1; j++) {
@@ -18,6 +20,7 @@ int main (int argc, char *argv[]) {
         printf("\n");
     }
     printf("\n");
+    // se realiza la matriz transpuesta y se imprime
     for (int i = 0; i < num1; i++) {
         for (int j = 0; j < num2; j++) {
             matriz[j][i];
